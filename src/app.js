dff1ac073814449128bdc4d7a4992526532b447c
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-08-07 23:32:50 +0000

    changed when const

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ function generateExcuse() {
     "right on time",
     "when I finished",
     "during my lunch",
-    "while I was praying"
+    "while I was sleeping"
   ];
 
   return `${who[Math.floor(Math.random() * who.length)]} ${
